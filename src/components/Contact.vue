1c191fc288dd88e957c525b27128f4603990f7d9
lohanyeon <yeonkh94@gmail.com>
2021-11-16 22:10:30 +0900

    add commit

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -64,7 +64,7 @@
                 <dd>
                   <strong>TEL.</strong> <a href="tel:02-333-9230">02.333.9230</a>
                   <span class="ml5"><strong>FAX.</strong> 02.6919.2699</span>
-                  <span class="ml5 m"><strong>e-mail.</strong> <a href="mainto:jacky@graviti.co.kr">jacky@graviti.co.kr</a></span>
+                  <span class="ml5 m"><strong>e-mail.</strong> <a href="mailto:jacky@graviti.co.kr">jacky@graviti.co.kr</a></span>
                 </dd>
               </dl>
               <!-- <p class="qr-code"><img src="/static/v2017/images/icon_qr-code.jpg" alt="그라비티인터랙티브 QR 코드"></p> -->
